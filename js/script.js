8afb2fcdcde9ab7b3a4454752b5f4001d414b712
Oshandi <piyumioshandi@gmail.com>
2023-01-17 09:28:54 -0500

    Updated script file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 
+//initial setup
+
+let a = 3;
+let b = 5;
+let c;
+
+let sum = a+b;
+let sub = a-b;
+let mul = a*b;
+
+let output = 'let a = 3;\n';
+output += 'let b = 5;\n';
+output += 'let c;\n';
+output += '________\n';
+output += 'a+b=' + sum + "\n";
+output += 'a-b=' + sub + "\n";
+output += 'a*b=' + mul + "\n";
+
+alert(output);
+
+/*
+alert('a+b=' + sum + "\n");
+alert('a-b=' + sub + "\n");
+alert('a*b=' + mul + "\n");
+*/
