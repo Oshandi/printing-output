507fd56de42af9b69be7e2ed37b6ed436399501e
Oshandi <piyumioshandi@gmail.com>
2023-01-23 20:36:15 -0500

    Completed Task 02

--- js/script.js
+++ js/script.js
@@ -37,3 +37,16 @@ outputOne += '!a && !c = ' + and + "\n";
 outputOne += '!a || !c = ' + or + "\n";
 
 alert(outputOne);
+
+/* ==========
+    Task 02
+========== */    
+
+let first_name = 'Piyumi Oshandi';
+let last_name = 'Dissanayake';
+let email = "diss0005@algonquinlive.com";
+
+let output;
+
+output = 'My name is ' + first_name + ' ' + last_name + '. ' + 'You can contact me at ' + email + '.';
+alert(output);
